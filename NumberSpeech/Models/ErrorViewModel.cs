81736aa7ae4c609796800ad32918b3666cc47540
Suhitkumar Maheshbhai Rathi <jrathisu@jci.com>
2020-05-30 12:21:25 +0530

    Add project files.

---
+++ NumberSpeech/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NumberSpeech.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
