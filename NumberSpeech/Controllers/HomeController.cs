d199e9fa00e427362e2bbbc8d86d7e7748006020
Suhitkumar Maheshbhai Rathi <jrathisu@jci.com>
2020-05-30 13:28:44 +0530

    controller method to convert number to text

--- NumberSpeech/Controllers/HomeController.cs
+++ NumberSpeech/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using NumberSpeech.Models;
 
@@ -17,13 +14,193 @@ namespace NumberSpeech.Controllers
 
         public string NumberToText(string number)
         {
-            return number;
+            return ConvertWholeNumber(number);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Convert as per international standard
+        private static String ConvertWholeNumber(String Number)
+        {
+            string word = "";
+            try
+            {
+                bool isDone = false;
+                double dblAmt = (Convert.ToDouble(Number));
+                if (dblAmt > 0)
+                {
+                    int numDigits = Number.Length;
+                    int pos = 0;
+                    String place = "";
+                    switch (numDigits)
+                    {
+                        case 1:
+
+                            word = ones(Number);
+                            isDone = true;
+                            break;
+                        case 2:
+                            word = tens(Number);
+                            isDone = true;
+                            break;
+                        case 3:
+                            pos = (numDigits % 3) + 1;
+                            place = " Hundred ";
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                            pos = (numDigits % 4) + 1;
+                            place = " Thousand ";
+                            break;
+                        case 7:
+                        case 8:
+                        case 9:
+                            pos = (numDigits % 7) + 1;
+                            place = " Million ";
+                            break;
+                        case 10:
+                        case 11:
+                        case 12:
+                            pos = (numDigits % 10) + 1;
+                            place = " Billion ";
+                            break;
+                        default:
+                            isDone = true;
+                            break;
+                    }
+                    if (!isDone)
+                    {
+                        if (Number.Substring(0, pos) != "0" && Number.Substring(pos) != "0")
+                        {
+                            try
+                            {
+                                word = ConvertWholeNumber(Number.Substring(0, pos)) + place + ConvertWholeNumber(Number.Substring(pos));
+                            }
+                            catch { }
+                        }
+                        else
+                        {
+                            word = ConvertWholeNumber(Number.Substring(0, pos)) + ConvertWholeNumber(Number.Substring(pos));
+                        }
+                    }
+                    if (word.Trim().Equals(place.Trim())) word = "";
+                }
+            }
+            catch { }
+            return word.Trim();
+        }
+
+        private static String tens(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = null;
+            switch (_Number)
+            {
+                case 10:
+                    name = "Ten";
+                    break;
+                case 11:
+                    name = "Eleven";
+                    break;
+                case 12:
+                    name = "Twelve";
+                    break;
+                case 13:
+                    name = "Thirteen";
+                    break;
+                case 14:
+                    name = "Fourteen";
+                    break;
+                case 15:
+                    name = "Fifteen";
+                    break;
+                case 16:
+                    name = "Sixteen";
+                    break;
+                case 17:
+                    name = "Seventeen";
+                    break;
+                case 18:
+                    name = "Eighteen";
+                    break;
+                case 19:
+                    name = "Nineteen";
+                    break;
+                case 20:
+                    name = "Twenty";
+                    break;
+                case 30:
+                    name = "Thirty";
+                    break;
+                case 40:
+                    name = "Fourty";
+                    break;
+                case 50:
+                    name = "Fifty";
+                    break;
+                case 60:
+                    name = "Sixty";
+                    break;
+                case 70:
+                    name = "Seventy";
+                    break;
+                case 80:
+                    name = "Eighty";
+                    break;
+                case 90:
+                    name = "Ninety";
+                    break;
+                default:
+                    if (_Number > 0)
+                    {
+                        name = tens(Number.Substring(0, 1) + "0") + " " + ones(Number.Substring(1));
+                    }
+                    break;
+            }
+            return name;
+        }
+
+        private static String ones(String Number)
+        {
+            int _Number = Convert.ToInt32(Number);
+            String name = "";
+            switch (_Number)
+            {
+
+                case 1:
+                    name = "One";
+                    break;
+                case 2:
+                    name = "Two";
+                    break;
+                case 3:
+                    name = "Three";
+                    break;
+                case 4:
+                    name = "Four";
+                    break;
+                case 5:
+                    name = "Five";
+                    break;
+                case 6:
+                    name = "Six";
+                    break;
+                case 7:
+                    name = "Seven";
+                    break;
+                case 8:
+                    name = "Eight";
+                    break;
+                case 9:
+                    name = "Nine";
+                    break;
+            }
+            return name;
         }
     }
 }
